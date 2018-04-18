16b54a1d73fe03e93915bf01ca43c361ab6d844c
Shubham <shubhamkant01@gmail.com>
2018-04-18 14:05:51 +0000

    puts serach node

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -2,3 +2,8 @@ file 'default www' do
         path '/var/www/html/index.html'
         content 'Hello world!'
 end
+#webnodes = search('node', 'role:web')
+#
+#webnodes.each do |node|
+#	puts node 
+#end
